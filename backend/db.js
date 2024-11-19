ec57548eb1e315700e65e579ecd89ae86e0f8a0a
Robi Firmansyah <robifirmansyah@RobitekiMacBook-Air.local>
2024-11-19 15:03:49 +0700

    set auto save

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql2');
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'admin',
-  password: 'admin123',
+  host: '10.7.90.63',
+  user: 'root',
+  password: 'my-secret-pw',
   database: 'support_ticket_db',
+  // host: 'localhost',
+  // user: 'admin',
+  // password: 'admin123',
+  // database: 'support_ticket_db',
 });
 db.connect(err => {
   if (err) throw err;
